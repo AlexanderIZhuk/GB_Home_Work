7b443e707059790168b7d85f55babf52cb92e973
AlexanderIZhuk <103458141+AlexanderIZhuk@users.noreply.github.com>
2022-04-21 07:40:41 +0500

    Delete Home_work_1 directory

--- Home_work_1/task_3.py
+++
@@ -1,7 +0,0 @@
-for i in range(1, 101):
-    if i % 10 > 4 and i % 10 <= 10 or i % 10 == 0 or i > 10 and i < 20:
-        print(i, "Процентов")
-    elif i % 10 > 1 and i % 10 < 5 :
-        print(i, "Процента")
-    elif i % 10 == 1:
-        print(i, "Процент")
